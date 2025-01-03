cde8030d0782a1b2912b5ec33d524744a75920ef
Aarnav Arya <officialaarnavarya4@gmail.com>
2025-01-03 22:32:56 +0530

    presciption.js

--- backend/models/prescription.js
+++ backend/models/prescription.js
@@ -1,1 +1,58 @@
+const mongoose = require('mongoose');
 
+const prescriptionSchema = new mongoose.Schema({
+  prescriptionId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hospitalName: {
+    type: String,
+    required: true
+  },
+  patientName: {
+    type: String,
+    required: true
+  },
+  patientAge: {
+    type: Number,
+    required: true
+  },
+  symptoms: [{
+    type: String
+  }],
+  diagnosis: {
+    type: String,
+    required: true
+  },
+  medicines: [{
+    name: {
+      type: String,
+      required: true
+    },
+    dosage: {
+      type: String,
+      required: true
+    },
+    dispensed: {
+      type: Boolean,
+      default: false
+    },
+    dispensedTime: Date
+  }],
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: String,
+    enum: ['active', 'expired', 'fulfilled'],
+    default: 'active'
+  },
+  expiryDate: {
+    type: Date,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Prescription', prescriptionSchema);
